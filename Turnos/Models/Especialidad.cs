778504918fa2fb8bfeb74907641a7f78d6ae0118
canepaluis <canepaluis9@gmail.com>
2021-09-19 21:15:06 -0500

    Initial commit

---
+++ Turnos/Models/Especialidad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Turnos.Models
+{
+    public class Especialidad
+    {
+        [Key]
+        public int IdEspecialidad { get; set;  }
+
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Turnos/Models/TurnosContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Turnos.Models
+{
+    public class TurnosContext : DbContext
+    {
+        public TurnosContext(DbContextOptions<TurnosContext> opciones) : base(opciones)
+        {
+
+        }
+
+        public DbSet<Especialidad> Especialidad { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Especialidad>(entidad =>
+            {
+                entidad.ToTable("Especialidad");
+                
+                entidad.HasKey(e => e.IdEspecialidad);
+                
+                entidad.Property(e => e.Descripcion)
+                .IsRequired()
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            });
+        }
+    }
+}

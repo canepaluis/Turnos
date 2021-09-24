edd7ed101852dbb93e7b126befd565d8216d19df
canepaluis <canepaluis9@gmail.com>
2021-09-24 18:01:02 -0500

    Avance tutorial

--- Turnos/Models/Especialidad.cs
+++ Turnos/Models/Especialidad.cs
@@ -9,8 +9,8 @@ namespace Turnos.Models
     public class Especialidad
     {
         [Key]
-        public int IdEspecialidad { get; set;  }
-
+        public int IdEspecialidad { get; set; }
         public string Descripcion { get; set; }
+        public List<MedicoEspecialidad> MedicoEspecialidad { get; set; }
     }
 }
